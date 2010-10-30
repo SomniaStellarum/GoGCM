d39a435e48dd11518772bbd9b1f707a8cb5c68ca
Arrow <bloggingarrow@gmail.com>
2010-10-30 13:01:03 -0400

    Update main model with latest example. Set up package structure.

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 include $(GOROOT)/src/Make.inc
 
 TARG=gcm
+DEPS=atmos
+GOFMT=gofmt -spaces=true -tabindent=false -tabwidth=4
 
 GOFILES=\
     gcm.go\
     
 include $(GOROOT)/src/Make.cmd
 
+format:
+	${GOFMT} -w gcm.go
--- gcm.go
+++ gcm.go
@@ -1,52 +1,71 @@
 package main
 
 import (
+    atm "github.com/Arrow/GoGCM/atmos"
     "fmt"
-    "rand"
+    "math"
 )
 
-const (
-    Mean = 17.5
-)
-
-type datapoint struct {
-    count int
-    temp  float64
-}
-
-func (dt datapoint) String() string {
-    return fmt.Sprintf("%d,%f\n", dt.count, dt.temp)
-}
-
-func counter() (ch chan int) {
-    ch = make(chan int)
-    go func() {
-        for i := 0; i <= 100; i++ {
-            ch <- i
-        }
-        close(ch)
-    }()
-    return ch
-}
-
-func gcm(ch chan int) (out chan datapoint) {
-    out = make(chan datapoint)
-    go func() {
-        dt := new(datapoint)
-        for c := range ch {
-            dt.count = c
-            dt.temp = Mean + rand.NormFloat64()
-            out <- *dt
-        }
-        close(out)
-    }()
-    return out
-}
 
 func main() {
-    ch := counter()
-    inChan := gcm(ch)
-    for in := range inChan {
-        fmt.Print(in)
+    dt := make([]atm.Datapoint, 4)
+
+    for i := 0; i < 4; i++ {
+        t := make([]float64, 3)
+        t[0] = 335 //AbsZero + 60
+        t[1] = 303 //AbsZero + 30
+        t[2] = 254 //AbsZero - 20
+        f := make([]float64, 9)
+        dt[i] = *atm.NewDatapoint(t, f)
+        fmt.Print(&dt[i])
+        dt[i].Idx = i
+        dt[i].Area = atm.KM * math.Pi * math.Pow(atm.EarthR, 2) // 4 pi r^2 / (4 gridpoints)
+        dt[i].BoundL = []float64{atm.KM * math.Pi * atm.EarthR / 2, atm.KM * math.Pi * atm.EarthR / 2, atm.KM * math.Pi * atm.EarthR / 2}
+        dt[i].Cp = []float64{atm.CpConst, atm.CpConst, atm.CpConst}
+        dt[i].K = atm.KConst
+        dt[i].A = atm.A
+    }
+    dt[0].Lat = 45.0
+    dt[0].Long = -45.0
+    dt[0].NPole = true
+    dt[1].Lat = 45.0
+    dt[1].Long = 45.0
+    dt[1].NPole = true
+    dt[2].Lat = -45.0
+    dt[2].Long = -45.0
+    dt[2].SPole = true
+    dt[3].Lat = -45.0
+    dt[3].Long = 45.0
+    dt[3].SPole = true
+
+    FluxIn := &atm.FluxInput{4, []int{2, 3, 4, 1, 3, 4, 1, 2, 4, 1, 2, 3}, []int{0, 3, 3, 6, 6, 9, 9, 12},
+        make(chan *atm.Datapoint), make(chan *atm.Datapoint)}
+
+    ch1 := FluxIn.ChFlux1
+    ch2 := FluxIn.ChFlux2
+
+    GCMIn := &atm.GCMInput{4, make(chan *atm.Datapoint)}
+    ch3 := GCMIn.Ch
+
+    out1 := atm.Flux(*FluxIn)
+    out2 := atm.Gcm(*GCMIn)
+    for i := 0; i < 10; i++ {
+        // Running the simulation
+        fmt.Printf("Run %d!\n", i)
+        for _, d := range dt {
+            ch1 <- &d
+            <-out1
+        }
+        for _, d := range dt {
+            ch2 <- &d
+            <-out1
+        }
+        for _, d := range dt {
+            ch3 <- &d
+            <-out2
+        }
+        for _, d := range dt {
+            fmt.Print(&d)
+        }
     }
 }
